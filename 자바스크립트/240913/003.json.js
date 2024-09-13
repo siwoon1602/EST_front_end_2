adc892064b3072eb6b928b3f4b58f6e994503a10
siwoon.yoon <siwoon1602@gmail.com>
2024-09-13 14:11:22 +0900

    :art: set수정,json실습중

---
+++ 자바스크립트/240913/003.json.js
@@ -0,0 +1,8 @@
+const bookList =
+  '[{"id":1,"title":"The Great Gatsby","author":"F. Scott Fitzgerald","year":1925,"genre":"Novel","available":true},{"id":2,"title":"To Kill a Mockingbird","author":"Harper Lee","year":1960,"genre":"Novel","available":false},{"id":3,"title":"1984","author":"George Orwell","year":1949,"genre":"Dystopian","available":true},{"id":4,"title":"Pride and Prejudice","author":"Jane Austen","year":1813,"genre":"Romance","available":true},{"id":5,"title":"The Catcher in the Rye","author":"J.D. Salinger","year":1951,"genre":"Novel","available":false}]';
+
+const book_ok = JSON.parse(bookList);
+const result = book_ok
+  .filter((book) => book.available)
+  .map((book) => book.title);
+console.log(result);
