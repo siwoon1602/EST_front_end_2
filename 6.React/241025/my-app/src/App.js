af07dee7202cfd58b3250aa6bceec74163988b97
siwoon.yoon <siwoon1602@gmail.com>
2024-10-25 10:28:08 +0900

    :memo" hook 실습 답안

---
+++ 6.React/241025/my-app/src/App.js
@@ -0,0 +1,18 @@
+import MenuList from "./Components/MenuList/MenuList";
+import DisplayMood from "./Components/DisplayMood/DisplayMood";
+import { useState } from "react";
+
+function App() {
+  const [currentMood, setCurrentMood] = useState("");
+
+  return (
+    <div>
+      <h1>오늘의 기분을 선택해주세요!</h1>
+      <article>
+        <MenuList setMood={setCurrentMood} mood={currentMood} />
+        <DisplayMood mood={currentMood} />
+      </article>
+    </div>
+  );
+}
+export default App;
---
+++ 6.React/241025/my-app/src/Components/MenuList/MenuList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import MenuListItem from "../MenuListItem/MenuListItem";
+
+export default function MenuList({ setMood, mood }) {
+  // 기분 상태 리스트
+  const menus = [
+    "좋아요! 😃",
+    "정말 좋아요! 🤭",
+    "최고에요! 😄",
+    "미쳤어요!! 🤪",
+  ];
+
+  return (
+    <ul className="container-list">
+      {menus.map((moodEl, index) => {
+        return (
+          <MenuListItem
+            isSelected={mood === moodEl}
+            key={index}
+            mood={moodEl}
+            setMood={setMood}
+          />
+        );
+      })}
+    </ul>
+  );
+}
