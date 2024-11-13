a0aa5b947f0abcfa5241e5d9f153547c77fdbe60
siwoon.yoon <siwoon1602@gmail.com>
2024-11-13 11:02:03 +0900

    🎉 webpack!

---
+++ 9.WEBPACK/webpackTest/src/app.js
@@ -0,0 +1,12 @@
+import { plus } from "./plus.js";
+
+console.log(plus(3, 4) + "입니다!");
+// const obj = {};
+
+// // (() => {
+//  export  function plus(a, b) {
+//     return a + b;
+//   }
+
+// //   obj.plus = plus;
+// // })();
---
+++ 9.WEBPACK/webpackTest2/src/app.js
@@ -0,0 +1,3 @@
+import { minus } from "./minus";
+
+console.log("3-4는" + minus(3, 4) + "입니다!");
